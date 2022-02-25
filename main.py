46ccea7726e66b75148b10aae0a6778f642717aa
jgoa156 <rockband5.0@hotmail.com>
2022-02-25 00:57:47 -0400

    Added support for multiple controllers

--- main.py
+++ main.py
@@ -1,64 +1,102 @@
+from charset_normalizer import detect
 import pygame
 from playsound import playsound
-
 import concurrent.futures
 import asyncio
 
+# CONSTS
+TICKRATE = 100000
+MAX_SAMPLE_THREADS = 50
+
+# GLOBALS
+controllers = []
+baseDir = "./samples/"
+defaultMap = {
+    0: {
+        "name": "green",
+        "sample": "stand.wav"
+    },
+    1: {
+        "name": "red",
+        "sample": "snare.wav"
+    },
+    2: {
+        "name": "blue",
+        "sample": "tom.wav"
+    },
+    3: {
+        "name": "yellow",
+        "sample": "hat.wav"
+    },
+    5: {
+        "name": "orange",
+        "sample": "crash.wav"
+    },
+}
+
+
 def play(file):
-	playsound(file)
+    playsound(file)
+
+
+class Controller:
+    def __init__(self, joystick, name, map={}):
+        print(f"New joystick detected - {name}")
+
+        self.joystick = joystick
+        self.name = name
+        self.map = map
+
+        self.executor = concurrent.futures.ThreadPoolExecutor(
+            MAX_SAMPLE_THREADS)
+        self.loop = asyncio.get_event_loop()
+        self.joystick.init()
+
+    def executeCommand(self, button):
+        if (button in self.map.keys()):
+            print(self.map[button])
+
+            file = baseDir + self.map[button]["sample"]
+            self.loop.run_in_executor(self.executor, play, file)
+
+
+def detectControllers():
+    joystickCount = 0
+
+    # while(True):
+    newCount = pygame.joystick.get_count()
+
+    if (joystickCount != newCount):
+        for index in range(joystickCount, newCount):
+            joystick = pygame.joystick.Joystick(index)
+
+            controllers.append(Controller(
+                joystick, joystick.get_name(), defaultMap))
+
+        joystickCount = newCount
+
+
+def listen():
+    clock = pygame.time.Clock()
+    clock.tick(TICKRATE)
+
+    while True:
+        event = pygame.event.get()
+
+        if (len(event) != 0 and event[0].type == pygame.JOYBUTTONDOWN):
+            controllers[event[0].joy].executeCommand(event[0].button)
+
 
 if __name__ == "__main__":
-	pygame.init()
-	joysticks = []
-	clock = pygame.time.Clock()
-	executor = concurrent.futures.ThreadPoolExecutor(50)
-	loop = asyncio.get_event_loop()
+    pygame.init()
 
-	# for al the connected joysticks
-	for i in range(0, pygame.joystick.get_count()):
-		# create an Joystick object in our list
-		joysticks.append(pygame.joystick.Joystick(i))
+    """
+    executor = concurrent.futures.ThreadPoolExecutor(2)
+    loop = asyncio.get_event_loop()
 
-		# initialize them all (-1 means loop forever)
-		joysticks[-1].init()
+    loop.run_in_executor(executor, detectControllers)
+    loop.run_in_executor(executor, listen)
+    """
 
-		# print a statement telling what the name of the controller is
-		print ("Detected joystick "),joysticks[-1].get_name(),"'"
-
-	baseDir = "./samples/"
-	buttonMap = {
-		0: {
-			"name": "green",
-			"sample": "stand.wav"
-		},
-		1: {
-			"name": "red",
-			"sample": "snare.wav"
-		},
-		2: {
-			"name": "blue",
-			"sample": "tom.wav"
-		},
-		3: {
-			"name": "yellow",
-			"sample": "hat.wav"
-		},
-		5: {
-			"name": "orange",
-			"sample": "crash.wav"
-		},
-	}
-  
-	while True:
-		clock.tick(1000)
-		
-		event = pygame.event.get()
-
-		if (len(event) != 0 and event[0].type == 1539):
-			button = vars(event[0])
-
-			if ("button" in button.keys()):
-				print(buttonMap[button["button"]])
-
-				file = baseDir + buttonMap[button["button"]]["sample"]
-				loop.run_in_executor(executor, play, file)
+    detectControllers()
+    listen()
